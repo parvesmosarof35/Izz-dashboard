e04f33ef02f69889dd530ceb44ed1bd0c1fa67ee
Raju Ahmmed <ahraju2003@gmail.com>
2025-12-29 02:03:47 +0600

    working profile

---
+++ src/redux/api/policy.js
@@ -0,0 +1,23 @@
+import { baseApi } from "../baseUrl";
+
+// auth api
+export const policyApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // create terms and condition
+    createPolicy: builder.mutation({
+      query: (credentials) => ({
+        url: "/policy",
+        method: "PATCH",
+        headers: {
+          Authorization: `${localStorage.getItem("accessToken")}`,
+        },
+        body: credentials,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetTermsAndConditionQuery,
+  useCreateTermsAndConditionMutation,
+} = policyApi;
