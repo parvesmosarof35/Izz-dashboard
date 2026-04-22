32e149eb387df881a549b318ce75445dfb1a14cc
Parves Mosarof <parvesmosarof35@gmail.com>
2026-04-23 01:20:18 +0600

    update base url

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://vat-component-billing-attendance.trycloudflare.com/api/v1",
+    baseUrl: "https://sports-izz-122-backend-bk39.onrender.com/api/v1",
     // baseUrl: "https://acknowledge-veterinary-care-josh.trycloudflare.com/api/v1",
   }),
   tagTypes: [
